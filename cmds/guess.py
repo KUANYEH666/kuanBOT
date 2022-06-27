10203b452d8c421f4fecfea8e7a94918e5aed53e
KUANYEH666 <105289169+KUANYEH666@users.noreply.github.com>
2022-06-27 08:31:20 +0800

    終極密碼&1A2B

--- cmds/guess.py
+++ cmds/guess.py
@@ -11,86 +11,86 @@ with open('setting.json', mode='r', encoding='utf8') as jfile:
 
 
 class guess(Cog_Extension):
-    @commands.command()
-    async def guess(self, ctx):
+    # @commands.command()
+    # async def guess(self, ctx):
     
-    # 檢查回傳的是否是同一個人(已及是否在同一個頻道)
-        def check(number):
-            return number.author == ctx.author and number.channel == ctx.message.channel
-        global lowernumber
-        global highernumber
+    # # 檢查回傳的是否是同一個人(已及是否在同一個頻道)
+    #     def check(number):
+    #         return number.author == ctx.author and number.channel == ctx.message.channel
+    #     global lowernumber
+    #     global highernumber
     
-        lowernumber = 1
-        highernumber = 100
+    #     lowernumber = 1
+    #     highernumber = 100
     
-        number = random.randint(lowernumber, highernumber)
-        print(number)
-    # print(number)
+    #     number = random.randint(lowernumber, highernumber)
+    #     print(number)
+    # # print(number)
     
-        embed=discord.Embed(title="終極密碼")
-        embed.set_author(name="歡迎遊玩終極密碼", icon_url="https://cdn.discordapp.com/attachments/915252108507365386/981748316213760040/1654137429311.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981434436367310939/hololive_vtuber_art_543811126_p_2849634115790321129_1_p_2849634115790321129.webp")
-        embed.add_field(name="請輸入", value="1~100中的數字", inline=False)
-        await ctx.send(embed=embed)
+    #     embed=discord.Embed(title="終極密碼")
+    #     embed.set_author(name="歡迎遊玩終極密碼", icon_url="https://cdn.discordapp.com/attachments/915252108507365386/981748316213760040/1654137429311.png")
+    #     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981434436367310939/hololive_vtuber_art_543811126_p_2849634115790321129_1_p_2849634115790321129.webp")
+    #     embed.add_field(name="請輸入", value="1~100中的數字", inline=False)
+    #     await ctx.send(embed=embed)
 
 
-        for i in range(10):    
-            response = await self.bot.wait_for('message', check = check)
+    #     for i in range(10):    
+    #         response = await self.bot.wait_for('message', check = check)
         
-            try : 
-                guess = int(response.content) 
+    #         try : 
+    #             guess = int(response.content) 
 
         
-            except:
-                embed=discord.Embed(title="e04")
-                embed.set_author(name="🛑錯誤!🛑")
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981746032583012372/9936c1bd85ee255b5d8adad1fb2f5c95.gif")
-                embed.add_field(name="不會玩終極密碼逆", value="輸入數字拉幹", inline=False)
-                await ctx.send(embed=embed)
+    #         except:
+    #             embed=discord.Embed(title="e04")
+    #             embed.set_author(name="🛑錯誤!🛑")
+    #             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981746032583012372/9936c1bd85ee255b5d8adad1fb2f5c95.gif")
+    #             embed.add_field(name="不會玩終極密碼逆", value="輸入數字拉幹", inline=False)
+    #             await ctx.send(embed=embed)
             
-            if guess == number : 
-                embed=discord.Embed(title="恭喜你")
-                embed.set_author(name="答對拉~", icon_url="https://cdn.discordapp.com/attachments/915252108507365386/981748316213760040/1654137429311.png")
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/980815090351759450/congratulations.gif")
-                embed.add_field(name="正確", value=f'答案就是{number}', inline=False)
-                await ctx.send(embed=embed)
-                break
+    #         if guess == number : 
+    #             embed=discord.Embed(title="恭喜你")
+    #             embed.set_author(name="答對拉~", icon_url="https://cdn.discordapp.com/attachments/915252108507365386/981748316213760040/1654137429311.png")
+    #             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/980815090351759450/congratulations.gif")
+    #             embed.add_field(name="正確", value=f'答案就是{number}', inline=False)
+    #             await ctx.send(embed=embed)
+    #             break
             
-            if guess > 100 :
-                embed=discord.Embed(title="e04")
-                embed.set_author(name="🛑錯誤!🛑")
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981746032583012372/9936c1bd85ee255b5d8adad1fb2f5c95.gif")
-                embed.add_field(name="不會玩終極密碼逆", value="大於100", inline=False)
-                await ctx.send(embed=embed)
+    #         if guess > 100 :
+    #             embed=discord.Embed(title="e04")
+    #             embed.set_author(name="🛑錯誤!🛑")
+    #             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981746032583012372/9936c1bd85ee255b5d8adad1fb2f5c95.gif")
+    #             embed.add_field(name="不會玩終極密碼逆", value="大於100", inline=False)
+    #             await ctx.send(embed=embed)
 
-            if guess < 1 :
-                embed=discord.Embed(title="e04")
-                embed.set_author(name="🛑錯誤!🛑")
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981746032583012372/9936c1bd85ee255b5d8adad1fb2f5c95.gif")
-                embed.add_field(name="不會玩終極密碼逆", value="小於1", inline=False)
-                await ctx.send(embed=embed)
+    #         if guess < 1 :
+    #             embed=discord.Embed(title="e04")
+    #             embed.set_author(name="🛑錯誤!🛑")
+    #             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981746032583012372/9936c1bd85ee255b5d8adad1fb2f5c95.gif")
+    #             embed.add_field(name="不會玩終極密碼逆", value="小於1", inline=False)
+    #             await ctx.send(embed=embed)
             
-            if 0 < guess < number:
-                lowernumber = guess
-                embed=discord.Embed(title="終極密碼")
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981434436367310939/hololive_vtuber_art_543811126_p_2849634115790321129_1_p_2849634115790321129.webp")
-                embed.add_field(name="請繼續", value="", inline=False)
-                embed.add_field(name=f'比{lowernumber}大比{highernumber}小', value="加油", inline=False)
-                await ctx.send(embed=embed)
+    #         if 0 < guess < number:
+    #             lowernumber = guess
+    #             embed=discord.Embed(title="終極密碼")
+    #             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981434436367310939/hololive_vtuber_art_543811126_p_2849634115790321129_1_p_2849634115790321129.webp")
+    #             embed.add_field(name="請繼續", value="", inline=False)
+    #             embed.add_field(name=f'比{lowernumber}大比{highernumber}小', value="加油", inline=False)
+    #             await ctx.send(embed=embed)
 
-            if 101 > guess > number :
-                highernumber = guess
-                embed=discord.Embed(title="終極密碼")
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981434436367310939/hololive_vtuber_art_543811126_p_2849634115790321129_1_p_2849634115790321129.webp")
-                embed.add_field(name="請繼續", value="", inline=False)
-                embed.add_field(name=f'比{lowernumber}大比{highernumber}小', value="加油", inline=False)
-                await ctx.send(embed=embed)
-        else:
-            embed=discord.Embed(title="差一點")
-            embed.set_author(name="GG", icon_url="https://cdn.discordapp.com/attachments/915252108507365386/981748316213760040/1654137429311.png")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/980815090351759450/congratulations.gif")
-            embed.add_field(name="可惜", value=f"答案是{number}", inline=False)
-            await ctx.send(embed=embed)
+    #         if 101 > guess > number :
+    #             highernumber = guess
+    #             embed=discord.Embed(title="終極密碼")
+    #             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981434436367310939/hololive_vtuber_art_543811126_p_2849634115790321129_1_p_2849634115790321129.webp")
+    #             embed.add_field(name="請繼續", value="", inline=False)
+    #             embed.add_field(name=f'比{lowernumber}大比{highernumber}小', value="加油", inline=False)
+    #             await ctx.send(embed=embed)
+    #     else:
+    #         embed=discord.Embed(title="差一點")
+    #         embed.set_author(name="GG", icon_url="https://cdn.discordapp.com/attachments/915252108507365386/981748316213760040/1654137429311.png")
+    #         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/980815090351759450/congratulations.gif")
+    #         embed.add_field(name="可惜", value=f"答案是{number}", inline=False)
+    #         await ctx.send(embed=embed)
 
     @commands.command()
     async def ab(self, ctx):
@@ -173,11 +173,11 @@ class guess(Cog_Extension):
         print(number)
     # print(number)
     
-        embed=discord.Embed(title="`1` ~ `100` 任意選一個數字", color=0xff0000, timestamp = datetime.datetime.now())
-        embed.set_author(name="🕹️ 娛樂中心 🕹️")
-        embed.set_thumbnail(url="https://cdn.dribbble.com/users/648258/screenshots/9070602/media/20cef9101ef2a1f1617b6b020ec97157.gif")
-        await ctx.message.delete()
-        await ctx.send(ctx.author.mention,embed=embed)
+        embed=discord.Embed(title="終極密碼")
+        embed.set_author(name="歡迎遊玩終極密碼", icon_url="https://cdn.discordapp.com/attachments/915252108507365386/981748316213760040/1654137429311.png")
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981434436367310939/hololive_vtuber_art_543811126_p_2849634115790321129_1_p_2849634115790321129.webp")
+        embed.add_field(name="請輸入", value="1~100中的數字", inline=False)
+        await ctx.send(embed=embed)
 
 
         for i in range(10):    
@@ -188,45 +188,53 @@ class guess(Cog_Extension):
 
         
             except:
-                embed=discord.Embed(title="🤔 ‖ 請輸入數字", color=0xff0000, timestamp = datetime.datetime.now())
-                embed.set_author(name="🕹️ 娛樂中心 🕹️")
-                embed.set_thumbnail(url="https://cdn.dribbble.com/users/648258/screenshots/9070602/media/20cef9101ef2a1f1617b6b020ec97157.gif")
-                await ctx.send(ctx.author.mention,embed=embed)
+                embed=discord.Embed(title="e04")
+                embed.set_author(name="🛑錯誤!🛑")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981746032583012372/9936c1bd85ee255b5d8adad1fb2f5c95.gif")
+                embed.add_field(name="不會玩終極密碼逆", value="輸入數字拉幹", inline=False)
+                await ctx.send(embed=embed)
             
             if guess == number : 
-                embed=discord.Embed(title="✅ ‖ 答對了!!", color=0xff0000, timestamp = datetime.datetime.now())
-                embed.set_author(name="🕹️ 娛樂中心 🕹️")
-                embed.set_thumbnail(url="https://c.tenor.com/uCPMfBXrypUAAAAC/win-minions.gif")
-                await ctx.send(ctx.author.mention,embed=embed)
+                embed=discord.Embed(title="恭喜你")
+                embed.set_author(name="答對拉~", icon_url="https://cdn.discordapp.com/attachments/915252108507365386/981748316213760040/1654137429311.png")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/980815090351759450/congratulations.gif")
+                embed.add_field(name="正確", value=f'答案就是{number}', inline=False)
+                await ctx.send(embed=embed)
                 break
             
             if guess > 100 :
-                embed=discord.Embed(title="❎ ‖ 超過 `100` ，格式錯誤", color=0xff0000, timestamp = datetime.datetime.now())
-                embed.set_author(name="🕹️ 娛樂中心 🕹️")
-                embed.set_thumbnail(url="https://cdn.dribbble.com/users/648258/screenshots/9070602/media/20cef9101ef2a1f1617b6b020ec97157.gif")
+                embed=discord.Embed(title="e04")
+                embed.set_author(name="🛑錯誤!🛑")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981746032583012372/9936c1bd85ee255b5d8adad1fb2f5c95.gif")
+                embed.add_field(name="不會玩終極密碼逆", value="大於100", inline=False)
                 await ctx.send(embed=embed)
 
             if guess < 1 :
-                embed=discord.Embed(title="❎ ‖ 低於 `1` ，格式錯誤", color=0xff0000, timestamp = datetime.datetime.now())
-                embed.set_author(name="🕹️ 娛樂中心 🕹️")
-                embed.set_thumbnail(url="https://cdn.dribbble.com/users/648258/screenshots/9070602/media/20cef9101ef2a1f1617b6b020ec97157.gif")
+                embed=discord.Embed(title="e04")
+                embed.set_author(name="🛑錯誤!🛑")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981746032583012372/9936c1bd85ee255b5d8adad1fb2f5c95.gif")
+                embed.add_field(name="不會玩終極密碼逆", value="小於1", inline=False)
                 await ctx.send(embed=embed)
             
             if 0 < guess < number:
                 lowernumber = guess
-                em1=discord.Embed(title=f"🤔 ‖ 比 `{lowernumber}` 大，比 `{highernumber}` 小", color=0xff0000, timestamp = datetime.datetime.now())
-                em1.set_author(name="🎮 終極密碼 🎮")
-                await ctx.send(embed=em1)
+                embed=discord.Embed(title="終極密碼")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981434436367310939/hololive_vtuber_art_543811126_p_2849634115790321129_1_p_2849634115790321129.webp")
+                embed.add_field(name="請繼續", value="", inline=False)
+                embed.add_field(name=f'比{lowernumber}大比{highernumber}小', value="加油", inline=False)
+                await ctx.send(embed=embed)
 
             if 101 > guess > number :
-                highernumber = guess
-                em2=discord.Embed(title=f"🤔 ‖ 比 `{lowernumber}` 大，比 `{highernumber}` 小", color=0xff0000, timestamp = datetime.datetime.now())
-                em2.set_author(name="🎮 終極密碼 🎮")
-                await ctx.send(embed=em2)
+                embed=discord.Embed(title="終極密碼")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/981434436367310939/hololive_vtuber_art_543811126_p_2849634115790321129_1_p_2849634115790321129.webp")
+                embed.add_field(name="請繼續", value="", inline=False)
+                embed.add_field(name=f'比{lowernumber}大比{highernumber}小', value="加油", inline=False)
+                await ctx.send(embed=embed)
         else:
-            embed=discord.Embed(title=f"GAME OVER\n答案為: `{number}`", color=0xff0000, timestamp = datetime.datetime.now())
-            embed.set_author(name="🕹️ 娛樂中心 🕹️")
-            embed.set_thumbnail(url="https://i.gifer.com/QeMS.gif")
+            embed=discord.Embed(title="差一點")
+            embed.set_author(name="GG", icon_url="https://cdn.discordapp.com/attachments/915252108507365386/981748316213760040/1654137429311.png")
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/915252108507365386/980815090351759450/congratulations.gif")
+            embed.add_field(name="可惜", value=f"答案是{number}", inline=False)
             await ctx.send(embed=embed)
 
 
